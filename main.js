4187eeca13051bb1d530b51658ba4b192c3274e1
angierobayo9809 <angierobayo09@gmail.com>
2023-02-14 19:01:04 -0500

    Operador lógico NOT '!'

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 let a = confirm("Variable A. \r\n Presiona Aceptar para asignarle valor true. \r\n Presiona Cancelar para asignarle valor false.")
-let b = confirm("Variable B. \r\n Presiona Aceptar para asignarle valor true. \r\n Presiona Cancelar para asignarle valor false.")
 
 let myTableBody = document.querySelector("#tableBody");
 myTableBody.insertAdjacentHTML("beforeend", `<tr id="trDynamic">
                                 <td>${a}</td>
-                                <td>${b}</td>
-                                <td>${a || b}</td>
+                                <td>${!a}</td>
                                 </tr>
                                 `);
